ff9c7857638cc3820e62ab6b19395733cfe65b3a
izaz97778 <izaz97778@gmail.com>
2026-05-28 12:22:23 +0500

    Fix formatting of uptime message in status_utils.py
    Signed-off-by: izaz97778 <izaz97778@gmail.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -303,5 +303,5 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
     buttons.data_button("♻️ Refresh", f"status {sid} ref", position="header")
     button = buttons.build_menu(8)
     msg += f"\n✦<b>CPU</b> » {cpu_percent()}% | <b>RAM</b> » {virtual_memory().percent}%"
-    msg += f"\n✦<b>FREE</b> » {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)} [{round(100 - disk_usage(DOWNLOAD_DIR).percent, 1)}%] \n🔹<b>UPTIME</b> » {get_readable_time(time() - bot_start_time)}"
+    msg += f"\n✦<b>FREE</b> » {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)} [{round(100 - disk_usage(DOWNLOAD_DIR).percent, 1)}%] \n✦<b>UPTIME</b> » {get_readable_time(time() - bot_start_time)}"
     return msg, button
